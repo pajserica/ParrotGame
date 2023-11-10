b92c7f8f610b5acae1601edb986c39ba3feeab50
zzmilan <milannikic04@gmail.com>
2023-11-10 14:55:50 +0100

    milan enters

--- Assets/_Scripts/Enemy/pajser/EnemyController.cs
+++ Assets/_Scripts/Enemy/pajser/EnemyController.cs
@@ -2,43 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(EnemyMovement))] 
-[RequireComponent(typeof(AttackPlayer))] 
+[RequireComponent(typeof(EnemyMovement))]
+//[RequireComponent(typeof(AttackPlayer))] 
 public class EnemyController : MonoBehaviour
 {
 
     EnemyMovement moveScript;
-    AttackPlayer attackScript;
+    //AttackPlayer attackScript;
     [SerializeField] List<Transform> patrollPoints;
-    private Transform objectEntered; 
+    private Transform objectEntered;
 
-    void Awake(){
+    void Awake()
+    {
         moveScript = GetComponent<EnemyMovement>();
-        attackScript = GetComponent<AttackPlayer>();
+        //attackScript = GetComponent<AttackPlayer>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    
-    public void GetInfo(Transform _transform){
-        objectEntered = _transform;
 
-        if(objectEntered.gameObject.tag == "Player"){ // place this in another func
-            moveScript.GoToPosition(objectEntered);
-            moveScript.followTransform(objectEntered);
-            if(!attackScript.isAttacking)
-                attackScript.Attack();
-        }
+    public void GetInfo(Transform _transform)
+    {
+        if (!_transform.CompareTag("Player"))
+            return;
+
+        moveScript.GoToPosition(_transform);
+        moveScript.followTransform(_transform);
+        //if (attackScript && !attackScript.isAttacking)
+            //attackScript.Attack();
     }
-
 }
--- Assets/_Scripts/Enemy/pajser/TriggerEnemy.cs
+++ Assets/_Scripts/Enemy/pajser/TriggerEnemy.cs
@@ -1,15 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TriggerEnemy : MonoBehaviour
 {
-    [SerializeField] EnemyController ctrlScript;
+    EnemyController ctrlScript;
 
+    private void Awake()
+    {
+        ctrlScript = transform.parent.GetComponent<EnemyController>();
+        print(ctrlScript.gameObject.name);
+    }
 
-    void OnTriggerEnter(Collider coll){
-        if(coll.tag == "Player"){
-            ctrlScript.GetInfo(coll.gameObject.transform);
-        }
+    void OnTriggerEnter(Collider coll)
+    {
+        ctrlScript.GetInfo(coll.transform);
     }
 }
